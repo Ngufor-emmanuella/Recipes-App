8fcb4527f90294be23b40504c345423528ed9318
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-03-02 01:57:39 +0100

    Fix linters errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,7 @@ class Food < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :measurement_unit, presence: true
-  validates :price, presence: true, numericality: {  greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   has_many :recipes_foods
   has_many :recipes, through: :recipes_foods
 end
