b0af93f494785ff596f2f470b372245d40cb8096
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-03-03 10:34:14 +0100

    Add recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  has_many :recipe_foods, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
   validates :public, presence: true
-  has_many :recipes_foods
-  has_many :foods, through: :recipes_foods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 end
