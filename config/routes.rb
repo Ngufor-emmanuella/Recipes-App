33c4e3d595774918580d6334c020fa6e29f7bca9
Samuellekpor <samuellekpor@gmail.com>
2023-03-04 00:54:12 +0000

    rou

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "foods#index"
   resources :users do
     resources :foods, only: [:index, :show, :new, :create, :destroy]
     resources :recipes, only: [:index, :show, :new, :create, :destroy, :public] do
       resources :recipe_foods, only: [:create, :new, :destroy]
     end
   end
-
+  # Defines the root path route ("/")
   get 'public_recipes' => 'recipes#public_recipes'
   resources :shoppings, only:[:index]
-
 end
