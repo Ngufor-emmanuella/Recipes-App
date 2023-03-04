581789f6d9606e9c0abc13b37ab02b4d2b18fbcc
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-03-04 10:49:32 +0100

    Add features

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,50 +1,39 @@
 class FoodsController < ApplicationController
-  # GET /foods or /foods.json
   def index
     @foods = Food.all
   end
 
-  # GET /foods/1 or /foods/1.json
-  def show; end
-
-  # GET /foods/new
   def new
     @food = Food.new
   end
 
-  def shopping_list
-    @foods = Food.all
-  end
-
-  # POST /foods or /foods.json
   def create
     @food = Food.new(food_params)
     @food.user = current_user
 
     if @food.save
       flash[:notice] = 'Food was successfully created'
-      redirect_to user_foods_path(:user_id)
+      redirect_to user_foods_path(params[:user_id])
     else
       render :new
     end
   end
 
-  # DELETE /foods/1 or /foods/1.json
-  def destroy
-    @food.destroy
+  def show
+    @food = Food.find(params[:id])
+  end
 
+  def shopping_list
+    @foods = Food.all
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
     flash[:notice] = 'Food was successfully deleted'
     redirect_to user_foods_path(params[:user_id])
   end
 
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_food
-    @food = Food.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  has_many :foods, through: :recipes_foods
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
